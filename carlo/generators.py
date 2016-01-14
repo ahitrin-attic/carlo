ebda1090ebd6012f4d0522b381b6c66b5792ab18
Andrey Hitrin <andrey.hitrin@gmail.com>
2016-01-14 16:47:20 +0500

    Fixed length and prefix

--- carlo/generators.py
+++ carlo/generators.py
@@ -1,13 +1,20 @@
 import random
 import string
 
+
 def int_val(fixed_value=None):
     return ('int', lambda: fixed_value)
 
-def string_val(fixed_value=None, length=None):
+
+def string_val(fixed_value=None, length=None, prefix=None):
     if fixed_value:
         return ('str', lambda: fixed_value)
-    return ('str', lambda: ''.join(random.choice(string.letters) for _ in range(length)))
+    if prefix:
+        length -= len(prefix)
+    else:
+        prefix = ''
+    return ('str', lambda: prefix + ''.join(random.choice(string.letters) for _ in range(length)))
+
 
 def time_val():
     pass
