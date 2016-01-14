7213d8a37771b6e49209adbebe93d301ef277ea6
Andrey Hitrin <andrey.hitrin@gmail.com>
2016-01-14 16:37:09 +0500

    Random string of given length

--- carlo/generators.py
+++ carlo/generators.py
@@ -1,8 +1,13 @@
+import random
+import string
+
 def int_val(fixed_value=None):
     return ('int', lambda: fixed_value)
 
-def string_val(fixed_value=None):
-    return ('str', lambda: fixed_value)
+def string_val(fixed_value=None, length=None):
+    if fixed_value:
+        return ('str', lambda: fixed_value)
+    return ('str', lambda: ''.join(random.choice(string.letters) for _ in range(length)))
 
 def time_val():
     pass
